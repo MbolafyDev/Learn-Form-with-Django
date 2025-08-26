2ade11156fc678588246244473e4213611e3f3bd
Mbolafy.Dev <mbolafy.dev@gmail.com>
2025-08-26 16:43:06 +0300

    Ajout d'autre champs et Migration vers la base de données

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,3 +5,5 @@ class ReviewForm(forms.Form):
         "required": "Your name must not be empty !",
         "max_length": "Username ne doit pas depasser le 100crts"
     })
+    review_text = forms.CharField(label="Your feed Back", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
