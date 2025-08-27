5e5a21902680b9193d1465ba9a8a6fa908b904b9
Mbolafy.Dev <mbolafy.dev@gmail.com>
2025-08-27 09:43:00 +0300

    Create View

--- reviews/views.py
+++ reviews/views.py
@@ -5,26 +5,43 @@ from django.views import View
 from django.views.generic.base import TemplateView
 from .models import Review
 from django.views.generic import ListView, DetailView
+# from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+class ReviewView(CreateView):
+    model = Review
+    # fields = "__all__"
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+# class ReviewView(FormView):
+#     form_class = ReviewForm
+#     template_name = "reviews/review.html"
+#     success_url = "/thank-you"
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+
+# class ReviewView(View):
+#     def get(self, request):
+#         form = ReviewForm()
+
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
     
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("thank-you")
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("thank-you")
         
-        return render(request, "reviews/review.html", {
-        "form": form
-        })
+#         return render(request, "reviews/review.html", {
+#         "form": form
+#         })
 
 class ThankYouView(TemplateView):
     # def get(self, request):
