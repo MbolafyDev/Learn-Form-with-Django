c082b0f028181f0ec6628b200268c02a71afab52
Mbolafy.Dev <mbolafy.dev@gmail.com>
2025-08-27 08:47:48 +0300

    Classe pour tous les view

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from django.views import View
+from django.views.generic.base import TemplateView
+from .models import Review
 
 # Create your views here.
 
@@ -23,5 +25,29 @@ class ReviewView(View):
         "form": form
         })
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    # def get(self, request):
+    #     return render(request, "reviews/thank_you.html")
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['message'] = "This Works!"
+        return context
+    
+class ReviewListView(TemplateView):
+    template_name = "reviews/review_list.html"
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context['reviews'] = reviews
+        return context
+    
+class ReviewDetailView(TemplateView):
+    template_name = "reviews/review_detail.html"
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        review_id = kwargs['id']
+        selected_review = Review.objects.get(pk=review_id)
+        context['review'] = selected_review
+        return context
